2ed6cee444b60dc4fdf8c6216a69f1973f340c60
Nicolas Gustavo <magrininicolas@outlook.com>
2021-03-04 12:39:18 -0300

    Primeiro commit

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#60C9EB';
+export const primaryDarkColor = '#1A1A1D';
+
+export const successColor = '#36EB36';
+export const infoColor = '#009AEB';
+export const errorColor = '#EB224B';
+export const warningColor = '#F2AF29';
---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragraph } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>SALVE</small>
+      </Title>
+      <Paragraph>Lorem ipsum dolor sit amet.</Paragraph>
+      <button type="button">Send</button>
+    </Container>
+  );
+}
